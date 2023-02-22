1726f17d8e8b9f27987e47c0a3b6b8ebc481a94c
escuderodev <escudero.ti@gmail.com>
2023-02-22 15:15:32 -0300

    teste implementação design patterns estrategy

--- DesignPatternsEstrategy/src/App.java
+++ DesignPatternsEstrategy/src/App.java
@@ -6,12 +6,10 @@ public class App {
     public static void main(String[] args) {
 
         Produto produto = new Produto("Kit de Brigadeiro com 12 unidades", 100);
-        FreteService frete = new MercadoEnvios();
+        FreteService frete = new Correios();
         DecimalFormat decimal = new DecimalFormat();
         decimal.applyPattern("0.00");
 
         System.out.println(String.format(String.format("\nO Produto " + produto.getNome() + " será entregue pela Empresa " + frete.getClass().getSimpleName() + " e o valor do frete será R$ " + decimal.format(frete.calculaFrete(produto.getPeso())) + ".")));
     }
 }
-
-//..
--- DesignPatternsEstrategy/src/br/com/escuderodev/models/Correios.java
+++ DesignPatternsEstrategy/src/br/com/escuderodev/models/Correios.java
@@ -3,6 +3,7 @@ package br.com.escuderodev.models;
 public class Correios implements FreteService {
     @Override
     public double calculaFrete(double peso) {
+
         return peso * 0.30;
     }
 }
