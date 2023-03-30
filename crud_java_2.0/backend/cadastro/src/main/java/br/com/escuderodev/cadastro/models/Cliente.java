addb058b7f29b9f458c0ce9ede0be3fb2d165591
Eduardo Escudero <escudero.ti@gmail.com>
2023-03-30 13:49:00 -0300

    iniciando backend

---
+++ crud_java_2.0/backend/cadastro/src/main/java/br/com/escuderodev/cadastro/models/Cliente.java
@@ -0,0 +1,86 @@
+package br.com.escuderodev.cadastro.models;
+
+public class Cliente {
+    private Integer idcliente;
+    private String nome;
+    private String telefone;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String estado;
+    private String cep;
+
+//    getters and setters
+    public Integer getIdcliente() {
+        return idcliente;
+    }
+
+    public void setIdcliente(Integer idcliente) {
+        this.idcliente = idcliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}
