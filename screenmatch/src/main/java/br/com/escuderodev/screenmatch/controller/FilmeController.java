35ce9eab0cec7fc2255f93798cbd7ecc6063bf4e
escuderodev <escudero.ti@gmail.com>
2023-05-28 15:29:08 -0300

    commit initial

---
+++ screenmatch/src/main/java/br/com/escuderodev/screenmatch/controller/FilmeController.java
@@ -0,0 +1,15 @@
+package br.com.escuderodev.screenmatch.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/filmes")
+public class FilmeController {
+
+    @GetMapping
+    public void carregaPaginaCadastroDeFilmes() {
+
+    }
+}
